ce5b3adfff1c0b1c1bb6c2e32b660ae35c8b81c1
Szobek Ádám Márk <szobek.adam@gmail.com>
2024-10-14 23:01:52 +0200

    adhfalkjfdahjs

--- src/main/kotlin/application/MainSceneController.kt
+++ src/main/kotlin/application/MainSceneController.kt
@@ -1,9 +1,61 @@
 package main.kotlin.application
 
 import javafx.fxml.FXML
+import javafx.geometry.Point3D
+import javafx.scene.Group
+import javafx.scene.PerspectiveCamera
+import javafx.scene.SceneAntialiasing
+import javafx.scene.SubScene
 import javafx.scene.layout.AnchorPane
+import javafx.scene.paint.Color
+import javafx.scene.shape.Sphere
+import javafx.scene.transform.Rotate
+import javafx.scene.transform.Translate
 
-public class MainSceneController {
+
+class MainSceneController {
     @FXML
-    public lateinit var sphereView: AnchorPane;
+    lateinit var sphereView: AnchorPane;
+
+
+    lateinit var sphereSubScene: SubScene;
+    var objectParent:Group=Group();
+
+    fun initScene()
+    {
+        var sphere:Sphere=Sphere();
+        sphere.apply{
+            transforms.clear();
+            transforms.addAll(Translate(10.0,10.0,10.0));
+        }
+
+        var camera:PerspectiveCamera = PerspectiveCamera();
+        camera.apply {
+            nearClip=1.0;
+            farClip=100.0;
+
+            fieldOfView=45.0;
+
+            var direction: Point3D = Point3D(sphere.translateX-camera.translateX,sphere.translateY-camera.translateY,sphere.translateZ-camera.translateZ);
+            direction.normalize();
+            var rotation:Rotate =Rotate(0.0, Point3D(0.0, 1.0, 0.0));
+            var translate:Translate=Translate(0.0,0.0,0.0);
+            transforms.clear();
+            transforms.addAll(rotation,translate);
+        }
+
+        objectParent.children.add(sphere);
+        sphereSubScene = SubScene(objectParent, sphereView.width, sphereView.width, true, SceneAntialiasing.BALANCED);
+        sphereSubScene.camera=camera;
+        sphereSubScene.fill= Color.BLACK;
+
+        sphereView.children.add(sphereSubScene);
+
+        sphereView.layoutBoundsProperty().addListener() { _, oldValue, newValue ->
+            if (oldValue.width != newValue.width || oldValue.height != newValue.height) {
+                sphereSubScene.width=newValue.width;
+                sphereSubScene.height=newValue.height;
+            }
+        }
+    }
 }
--- src/main/kotlin/application/Window.kt
+++ src/main/kotlin/application/Window.kt
@@ -8,18 +8,34 @@ import javafx.stage.Stage
 
 class Window() : Application() {
 
+    init
+    {
+        if(mainWindow==null)
+            mainWindow=this;
+    }
+
+    companion object
+    {
+        var mainWindow:Window?=null;//a static window instance that should be the one instantiated by the application
+    }
+
     lateinit var controller: MainSceneController;
+    lateinit var stage:Stage;
+    lateinit var root: Parent;
 
     override fun start(primaryStage: Stage?) {
         var loader:FXMLLoader=FXMLLoader(this.javaClass.getResource("/layouts/MainScene.fxml"));
-        var root: Parent =loader.load();
+        root =loader.load();
         var scene: Scene=Scene(root);
-        var stage: Stage=Stage();
 
         controller=loader.getController();
 
+        stage=Stage();
         stage.scene=scene;
         stage.title="Glock sphere";
+
+        controller.initScene();
+
         stage.show();
     }
 }
