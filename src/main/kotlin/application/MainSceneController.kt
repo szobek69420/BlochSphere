a6a3317440217058665d55b91b01016c7c64cca6
Szobek Ádám Márk <szobek.adam@gmail.com>
2024-10-19 11:05:32 +0200

    sphere window has a view class now yeee
    sdaiufhakshfjhaskfjhdaskhfkasjhfajskhfakjshdfdlkjsahfkjlsahfkjldsahfjkldsahfjlkdsahfdakjshfjkdlsahfkjldsfldhakfdajskhfjdkhfjkdahfljksahlkjshafdjkashdjkshjkashfjdashfjkldsahdjkfhajbvhhasifdzhgaihgsdlfiuawhufgiowahiwuahfuwqgiufwiqufuiwhQEUFEWUWIQOAFEEAOLUFHEIWUQAFHIUWQAFERUIOWAfhalkjdhfjksahdfiuoawehbfaskdjfhiewaal

--- src/main/kotlin/application/MainSceneController.kt
+++ src/main/kotlin/application/MainSceneController.kt
@@ -1,70 +1,38 @@
 package main.kotlin.application
 
 import javafx.fxml.FXML
-import javafx.geometry.Point3D
-import javafx.scene.DirectionalLight
 import javafx.scene.Group
-import javafx.scene.PerspectiveCamera
 import javafx.scene.SceneAntialiasing
 import javafx.scene.SubScene
-import javafx.scene.effect.Light
-import javafx.scene.effect.Lighting
 import javafx.scene.layout.AnchorPane
 import javafx.scene.paint.Color
-import javafx.scene.shape.Sphere
-import javafx.scene.transform.Rotate
-import javafx.scene.transform.Translate
+import main.kotlin.view.SphereView
 
 
 class MainSceneController {
     @FXML
-    lateinit var sphereView: AnchorPane;
+    lateinit var sphereContainer: AnchorPane;
 
 
     lateinit var sphereSubScene: SubScene;
-    var objectParent:Group=Group();
+    lateinit var objectParent:Group;
+
+    lateinit var sphereView:SphereView;
 
     fun initScene()
     {
-        var sphere:Sphere=Sphere();
-        sphere.apply{
-            transforms.clear();
-            transforms.addAll(Translate(0.0,0.0,10.0));
-        }
+        sphereContainer.children.clear();
 
-        var camera:PerspectiveCamera = PerspectiveCamera(true);
-        camera.apply {
-            nearClip=1.0;
-            farClip=100.0;
-
-            fieldOfView=45.0;
-
-            var direction: Point3D = Point3D(sphere.translateX-camera.translateX,sphere.translateY-camera.translateY,sphere.translateZ-camera.translateZ);
-            direction.normalize();
-            var rotation:Rotate =Rotate(0.0, Point3D(0.0, 1.0, 0.0));
-            var translate:Translate=Translate(0.0,0.0,0.0);
-            transforms.clear();
-            transforms.addAll(rotation,translate);
-        }
-
-        var light:DirectionalLight = DirectionalLight();
-        light.apply{
-            var rotation:Rotate=Rotate(30.0,Point3D(1.0,0.0,0.0));
-            var translation:Translate=Translate(0.0,10.0,0.0);
-            transforms.clear();
-            transforms.addAll(rotation,translation);
-        }
-
-
-        objectParent.children.add(sphere);
-        objectParent.children.add(light);
-        sphereSubScene = SubScene(objectParent, sphereView.width, sphereView.width, true, SceneAntialiasing.BALANCED);
-        sphereView.children.clear();
-        sphereSubScene.camera=camera;
+        objectParent=Group();
+        sphereSubScene = SubScene(objectParent, sphereContainer.width, sphereContainer.width, true, SceneAntialiasing.BALANCED);
         sphereSubScene.fill= Color.BLACK;
 
-        sphereView.children.add(sphereSubScene);
-        sphereView.layoutBoundsProperty().addListener() { _, oldValue, newValue ->
+        sphereContainer.children.add(sphereSubScene);
+
+        sphereView=SphereView(sphereSubScene);
+
+        //resize children as well
+        sphereContainer.layoutBoundsProperty().addListener() { _, oldValue, newValue ->
             if (oldValue.width != newValue.width || oldValue.height != newValue.height) {
                 sphereSubScene.width=newValue.width;
                 sphereSubScene.height=newValue.height;
---
+++ src/main/kotlin/view/SphereView.kt
@@ -0,0 +1,60 @@
+package main.kotlin.view
+
+import javafx.geometry.Point3D
+import javafx.scene.*
+import javafx.scene.shape.Shape3D
+import javafx.scene.shape.Sphere
+import javafx.scene.transform.Rotate
+import javafx.scene.transform.Translate
+
+
+class SphereView(var subScene:SubScene) {
+    var camera: PerspectiveCamera;
+    var sphere: Shape3D;
+    var light: LightBase;
+
+    init{
+        sphere=Sphere();
+        sphere.apply{
+            transforms.clear();
+            transforms.addAll(Translate(0.0,0.0,10.0));
+        }
+
+        camera = PerspectiveCamera(true);
+        camera.apply {
+            nearClip=1.0;
+            farClip=100.0;
+
+            fieldOfView=45.0;
+
+            var direction: Point3D = Point3D(sphere.translateX-camera.translateX,sphere.translateY-camera.translateY,sphere.translateZ-camera.translateZ);
+            direction.normalize();
+            var rotation: Rotate = Rotate(0.0, Point3D(0.0, 1.0, 0.0));
+            var translate: Translate = Translate(0.0,0.0,0.0);
+            transforms.clear();
+            transforms.addAll(rotation,translate);
+        }
+
+        light = DirectionalLight();
+        light.apply{
+            var rotation: Rotate = Rotate(30.0, Point3D(1.0,0.0,0.0));
+            var translation: Translate = Translate(0.0,10.0,0.0);
+            transforms.clear();
+            transforms.addAll(rotation,translation);
+        }
+
+
+        var rootGroup:Group;
+        if(subScene.root is Group)
+        {
+            rootGroup=subScene.root as Group;
+
+            rootGroup.children.add(sphere);
+            rootGroup.children.add(light);
+
+            subScene.camera=camera;
+        }
+        else
+            System.err.println("SphereView: subScene.root must be or derived from Group to produce desirable results");
+    }
+}
