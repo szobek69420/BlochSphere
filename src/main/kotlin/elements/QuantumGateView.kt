a13dbd6b71a8032a30089b967fbeed9699850649
Szobek Ádám Márk <szobek.adam@gmail.com>
2024-10-24 21:06:26 +0200

    asdfhjkadshf

--- src/main/kotlin/elements/QuantumGateView.kt
+++ src/main/kotlin/elements/QuantumGateView.kt
@@ -1,10 +1,32 @@
 package main.kotlin.elements
 
+import javafx.event.EventHandler
+import javafx.scene.input.MouseEvent
 import javafx.scene.paint.Color
 import javafx.scene.shape.Rectangle
 
-class QuantumGateView(var text:String):Rectangle(50.0,50.0) {
+class QuantumGateView(var text:String, val _fill:Color, val isInCircuit:Boolean, val onGrab: (String, Boolean, MouseEvent)->Unit, val onDrag:(String, MouseEvent)->Unit):Rectangle(50.0,50.0) {
+
     init{
-        fill= Color.YELLOW;
+        fill=_fill;
+
+        val mouseHandler:MouseHandler=MouseHandler(this);
+        onMousePressed=mouseHandler;
+        onMouseReleased=mouseHandler;
+        onMouseDragged=mouseHandler;
+    }
+
+    class MouseHandler(val view:QuantumGateView):EventHandler<MouseEvent>{
+        override fun handle(event: MouseEvent?) {
+            if(event==null)
+                return;
+
+            when(event.eventType){
+                MouseEvent.MOUSE_PRESSED->view.onGrab(view.text,view.isInCircuit,event);
+                MouseEvent.MOUSE_RELEASED->view.onGrab(view.text,view.isInCircuit,event);
+                MouseEvent.MOUSE_DRAGGED->view.onDrag(view.text,event);
+            }
+        }
+
     }
 }
--- src/main/kotlin/view/CircuitView.kt
+++ src/main/kotlin/view/CircuitView.kt
@@ -1,13 +1,25 @@
 package main.kotlin.view
 
+import javafx.geometry.Point2D
+import javafx.scene.Node
+import javafx.scene.input.MouseEvent
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.FlowPane
+import javafx.scene.paint.Color
 import main.kotlin.elements.QuantumGateView
 import main.kotlin.elements.QuantumGateViewBackground
 import main.kotlin.elements.ResizableAnchorPane
 
 class CircuitView(val circuitContainer:AnchorPane, val operationContainer:FlowPane, val overlay:AnchorPane) {
     private val gatesInCircuit:ArrayList<String> = ArrayList<String>();
+
+    //drag data
+    private var inDrag:Boolean=false;
+    private var draggedText:String="TEMP";//the temporary value, that is automatically removed from the circuit on rerender if there is no drag in progress
+    private var fromCircuit:Boolean=false;
+    private var targetIndex:Int=-1;
+
+    private var asdf:QuantumGateViewBackground?=null;
 
     init{
         gatesInCircuit.add("sus");
@@ -19,6 +31,18 @@ class CircuitView(val circuitContainer:AnchorPane, val operationContainer:FlowPa
         }
 
         renderCircuit();
+
+        fillGateRegistry();
+    }
+
+    private fun fillGateRegistry()
+    {
+        val gates=arrayOf("X","Y","Z","H");
+
+        for(gate in gates)
+        {
+            operationContainer.children.add(QuantumGateView(gate,Color.YELLOW,false,{t,c,e->onQuantumGateGrab(t,c,e);}, {t,e->onQuantumGateDrag(t,e);}));
+        }
     }
 
     fun renderCircuit()
@@ -30,13 +54,13 @@ class CircuitView(val circuitContainer:AnchorPane, val operationContainer:FlowPa
         for (bill in gatesInCircuit)
         {
             //add background
-            val bg:QuantumGateViewBackground= QuantumGateViewBackground(index,circuitContainer.height){i,selected->onGateSlotSelected(i,selected);}
+            val bg:QuantumGateViewBackground= QuantumGateViewBackground(index,circuitContainer.height);
             circuitContainer.children.add(bg);
             AnchorPane.setLeftAnchor(bg,offset);
             AnchorPane.setBottomAnchor(bg,0.0);
 
             //add gate
-            val gate:QuantumGateView=QuantumGateView(bill);
+            val gate:QuantumGateView=QuantumGateView(bill, Color.YELLOW,true,{t,c,e->onQuantumGateGrab(t,c,e);}, {t,e->onQuantumGateDrag(t,e);});
             circuitContainer.children.add(gate);
             AnchorPane.setLeftAnchor(gate,offset+12.5);
             AnchorPane.setBottomAnchor(gate,0.5*(circuitContainer.height-gate.height));
@@ -44,11 +68,69 @@ class CircuitView(val circuitContainer:AnchorPane, val operationContainer:FlowPa
             offset+=75.0;
             index++;
         }
+
+        asdf=circuitContainer.children[0] as QuantumGateViewBackground;
     }
 
-    //if the user hovers a moved gate over a gate slot
-    fun onGateSlotSelected(index:Int,isSelected:Boolean)
+    private fun onQuantumGateGrab(text:String,inCircuit:Boolean,event:MouseEvent)
     {
-        println(index.toString()+" "+isSelected.toString());
+        if(fromCircuit==true)
+            return;
+
+        when(event.eventType)
+        {
+            MouseEvent.MOUSE_PRESSED->{
+                targetIndex=-1;
+                fromCircuit=inCircuit;
+                draggedText=text;
+                inDrag=true;
+            }
+
+            MouseEvent.MOUSE_RELEASED->{
+                if(!fromCircuit)
+                {
+                    if(targetIndex!=-1)
+                    {
+                        gatesInCircuit.add(targetIndex,draggedText);
+                        renderCircuit();
+                    }
+                }
+
+                targetIndex=-1;
+                draggedText="TEMP";
+                inDrag=false;
+            }
+        }
+    }
+
+    private fun onQuantumGateDrag(text:String,event:MouseEvent)
+    {
+        if(!inDrag)//habar ez valszeg nem fordul elo
+            return;
+
+        targetIndex=-1;
+        for(node: Node in circuitContainer.children)
+        {
+            if(node !is QuantumGateViewBackground)
+                continue;
+
+            val qgvb:QuantumGateViewBackground=node as QuantumGateViewBackground;
+            val lower=qgvb.localToScene(Point2D(0.0,0.0));
+            val upper=qgvb.localToScene(Point2D(qgvb.width,qgvb.height));
+
+            if(event.sceneX>lower.x&&event.sceneX<upper.x&&event.sceneY>lower.y&&event.sceneY<upper.y)
+                targetIndex=qgvb.index;
+        }
+
+        if(targetIndex==-1)
+        {
+            if(event.sceneX>circuitContainer.layoutBounds.minX
+                &&event.sceneX<circuitContainer.layoutBounds.maxX
+                &&event.sceneY>circuitContainer.layoutBounds.minY
+                &&event.sceneY<circuitContainer.layoutBounds.maxY)
+            {
+                targetIndex=gatesInCircuit.size;
+            }
+        }
     }
 }
