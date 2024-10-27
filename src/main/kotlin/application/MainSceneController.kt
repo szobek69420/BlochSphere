edc61b2def57026b93d3933970e0304010bb961e
Szobek Ádám Márk <szobek.adam@gmail.com>
2024-10-28 00:29:53 +0100

    custom titlebar

--- src/main/kotlin/application/MainSceneController.kt
+++ src/main/kotlin/application/MainSceneController.kt
@@ -2,11 +2,15 @@ package main.kotlin.application
 
 import javafx.event.EventHandler
 import javafx.fxml.FXML
+import javafx.geometry.Insets
 import javafx.scene.Group
 import javafx.scene.SceneAntialiasing
 import javafx.scene.SubScene
 import javafx.scene.control.Accordion
 import javafx.scene.control.ScrollPane
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
+import javafx.scene.input.MouseEvent
 import javafx.scene.layout.*
 import javafx.scene.paint.Color
 import main.kotlin.elements.BetterButton
@@ -18,7 +22,7 @@ import main.kotlin.view.SphereView
 class MainSceneController {
 
     @FXML
-    lateinit var root:AnchorPane;
+    lateinit var applicationRoot:AnchorPane;
 
     @FXML
     lateinit var toolboxAccordion: Accordion;
@@ -37,6 +41,16 @@ class MainSceneController {
     @FXML
     lateinit var circuitContainerScrollPaneContainer:AnchorPane;
 
+    //rescale things
+    @FXML
+    lateinit var menuBarDraggable:AnchorPane;
+    @FXML
+    lateinit var closeButton: AnchorPane;
+    @FXML
+    lateinit var rescaleButton:AnchorPane;
+    @FXML
+    lateinit var minimizeButton:AnchorPane;
+
 
 
     lateinit var sphereSubScene: SubScene;
@@ -46,9 +60,7 @@ class MainSceneController {
 
     lateinit var circuitView:CircuitView;
 
-    lateinit var exitButton:BetterButton;//ide kell a lateinit
-    lateinit var rescaleButton:BetterButton;
-    lateinit var minimizeButton:BetterButton;
+
 
     fun initScene()
     {
@@ -78,7 +90,7 @@ class MainSceneController {
 
         //circuit view
         circuitContainerScrollPaneContainer.maxHeight=200.0;
-        circuitView= CircuitView(circuitContainer,normieContainer,phaseContainer,root){q->onCircuitValueChange(q)};
+        circuitView= CircuitView(circuitContainer,normieContainer,phaseContainer,applicationRoot){q->onCircuitValueChange(q)};
 
         circuitContainerScrollPaneContainer.layoutBoundsProperty().addListener(){_,oldValue,newValue->
             if(oldValue.height!=newValue.height||oldValue.width!=newValue.width)
@@ -94,10 +106,78 @@ class MainSceneController {
             if(event!=null)
                 (circuitContainerScrollPaneContainer.children[0] as ScrollPane).hvalue-=10.0*event.deltaY/((circuitContainerScrollPaneContainer.children[0] as ScrollPane).content as Pane).width;
         }
+
+        //setting up rescale things
+        initMenuBarButtons();
+    }
+
+    fun changeRescaleImage(isMaximized:Boolean)
+    {
+        val imagePath:String=if(isMaximized) "/sprites/rescale2.png" else "/sprites/rescale1.png";
+        (rescaleButton.children[0] as ImageView).image=Image(this.javaClass.getResourceAsStream(imagePath));
     }
 
     private fun onCircuitValueChange(value: Qubit)
     {
         sphereView.setValue(value);
+    }
+
+    private fun initMenuBarButtons()
+    {
+        val mh_close=MenuBarButtonMouseHandler(closeButton,Color.TRANSPARENT,Color.RED,Color(1.0,0.0,0.0,0.7)){Window.mainWindow?.closeApplication()}
+        val mh_rescale=MenuBarButtonMouseHandler(rescaleButton,Color.TRANSPARENT,Color(0.6,0.6,0.6,1.0),Color(0.6,0.6,0.6,0.7)){Window.mainWindow?.rescaleApplication()}
+        val mh_minimize=MenuBarButtonMouseHandler(minimizeButton,Color.TRANSPARENT,Color(0.6,0.6,0.6,1.0),Color(0.6,0.6,0.6,0.7)){Window.mainWindow?.minimizeApplication()}
+
+        val handlers= arrayOf(mh_close,mh_rescale,mh_minimize);
+        val images= arrayOf(closeButton.children[0] as ImageView,rescaleButton.children[0] as ImageView, minimizeButton.children[0] as ImageView);
+
+        for(i in 0..2)
+        {
+            images[i].onMouseEntered=handlers[i];
+            images[i].onMouseExited=handlers[i];
+            images[i].onMousePressed=handlers[i];
+            images[i].onMouseReleased=handlers[i];
+        }
+    }
+
+    private class MenuBarButtonMouseHandler(val background:AnchorPane, val normalColour:Color, val hoverColour:Color, val pressedColour:Color, val onClick:()->Unit):EventHandler<MouseEvent>{
+        private var isEntered=false;
+        private var isPressed=false;
+
+        override fun handle(event: MouseEvent?) {
+            if(event==null)
+                return;
+
+            when(event.eventType)
+            {
+                MouseEvent.MOUSE_ENTERED->{
+                    isEntered=true;
+
+                    if(isPressed)
+                        background.background= Background(BackgroundFill(pressedColour, CornerRadii.EMPTY, Insets.EMPTY));
+                    else
+                        background.background= Background(BackgroundFill(hoverColour, CornerRadii.EMPTY, Insets.EMPTY));
+                }
+                MouseEvent.MOUSE_EXITED->{
+                    isEntered=false;
+                    background.background= Background(BackgroundFill(normalColour, CornerRadii.EMPTY, Insets.EMPTY));
+                }
+                MouseEvent.MOUSE_PRESSED->{
+                    isPressed=true;
+                    background.background= Background(BackgroundFill(pressedColour, CornerRadii.EMPTY, Insets.EMPTY));
+                }
+                MouseEvent.MOUSE_RELEASED->{
+                    isPressed=false;
+                    background.background= Background(BackgroundFill(normalColour, CornerRadii.EMPTY, Insets.EMPTY));//direkt normalcoloru
+
+                    if(isEntered)
+                    {
+                        isEntered=false;
+                        onClick();
+                    }
+                }
+            }
+        }
+
     }
 }
--- src/main/kotlin/application/Window.kt
+++ src/main/kotlin/application/Window.kt
@@ -46,7 +46,7 @@ class Window() : Application() {
         stage=Stage();
         stage.scene=scene;
         stage.title="Morbisoft's Glock Sphere";
-        stage.initStyle(StageStyle.DECORATED);
+        stage.initStyle(StageStyle.UNDECORATED);
 
         val icon:Image= Image(this.javaClass.getResourceAsStream("/sprites/icon.png"));
         stage.icons.add(icon);
@@ -56,5 +56,183 @@ class Window() : Application() {
         stage.show();
 
         stage.width+=1.0;//to trigger a layout reload, so that the circuit renders properly
+
+        val mouseHandler:MouseEventHandler=MouseEventHandler(stage){event->onApplicationRescale(event);};//a stage show utan kell letrehozni, mert konstruktorban lekeri a stage mereteit
+        controller.menuBarDraggable.onMousePressed=mouseHandler;
+        controller.menuBarDraggable.onMouseDragged=mouseHandler;
+        controller.menuBarDraggable.onMouseReleased=mouseHandler;
+    }
+
+    fun closeApplication()
+    {
+        stage.close();
+    }
+
+    fun minimizeApplication()
+    {
+        stage.isIconified=true;
+    }
+
+
+    fun rescaleApplication()
+    {
+        val handler:MouseEventHandler=controller.menuBarDraggable.onMouseDragged as MouseEventHandler;
+
+        if(handler.screenState==MouseEventHandler.ScreenState.FULL)
+            handler.desiredScreenState=MouseEventHandler.ScreenState.NORMAL;
+        else
+            handler.desiredScreenState=MouseEventHandler.ScreenState.FULL;
+
+        handler.adjustScreenState(null);
+        controller.changeRescaleImage(handler.screenState==MouseEventHandler.ScreenState.FULL);//change the icon of the rescale button
+    }
+
+    private fun onApplicationRescale(handler:MouseEventHandler)
+    {
+        controller.changeRescaleImage(handler.screenState==MouseEventHandler.ScreenState.FULL);
+    }
+
+    //needed to move around my borderless window
+    class MouseEventHandler(private val stage:Stage, private val onRescale:((MouseEventHandler)->Unit)?) : EventHandler<MouseEvent>
+    {
+        private var offsetX:Double=0.0;
+        private var offsetY:Double=0.0;
+
+        private var previousOffsetX:Double=0.0;
+        private var previousOffsetY:Double=0.0;
+        private var previousWidth:Double=0.0;
+        private var previousHeight:Double=0.0;
+
+        //where does the screen orient itself
+        enum class ScreenState{
+            NORMAL, FULL, LEFT, RIGHT
+        };
+
+        var screenState:ScreenState=ScreenState.NORMAL;
+        var desiredScreenState:ScreenState=ScreenState.NORMAL;
+        private val SCREEN_STAGE_CHANGE_THRESHOLD:Double=2.0;//if the mouse is within 2 pixels of the screens top/left/right side, the screen will want to change state
+
+        init{
+            previousWidth=stage.width;
+            previousHeight=stage.height;
+        }
+
+        override fun handle(event: MouseEvent?) {
+            if(event==null)
+                return;
+
+            when(event.eventType)
+            {
+                MouseEvent.MOUSE_DRAGGED->{
+
+                    var shouldRealignToNormal:Boolean=false;//if the screen is oriented somewhere but it doesn't meet the criteria for said orientation
+                    when(screenState)
+                    {
+                        ScreenState.FULL->{
+                            if(stage.y>Screen.getPrimary().visualBounds.minY+SCREEN_STAGE_CHANGE_THRESHOLD)
+                                shouldRealignToNormal=true;
+                        }
+                        ScreenState.LEFT->{
+                            if(stage.x>Screen.getPrimary().visualBounds.minX+SCREEN_STAGE_CHANGE_THRESHOLD)
+                                shouldRealignToNormal=true;
+                        }
+                        ScreenState.RIGHT->{
+                            if(stage.x+stage.width<Screen.getPrimary().visualBounds.maxX-SCREEN_STAGE_CHANGE_THRESHOLD)
+                                shouldRealignToNormal=true;
+                        }
+
+                        ScreenState.NORMAL -> {
+                            if(event.screenY<Screen.getPrimary().visualBounds.minY+SCREEN_STAGE_CHANGE_THRESHOLD)
+                                desiredScreenState=ScreenState.FULL;
+                            else if(event.screenX<Screen.getPrimary().visualBounds.minX+SCREEN_STAGE_CHANGE_THRESHOLD)
+                                desiredScreenState=ScreenState.LEFT;
+                            else if(event.screenX>Screen.getPrimary().visualBounds.maxX-SCREEN_STAGE_CHANGE_THRESHOLD)
+                                desiredScreenState=ScreenState.RIGHT;
+                            else
+                                desiredScreenState=ScreenState.NORMAL;
+                        }
+                    }
+
+                    if(shouldRealignToNormal)
+                    {
+                        offsetX=previousOffsetX;
+                        offsetY=previousOffsetY;
+
+                        stage.width=previousWidth;
+                        stage.height=previousHeight;
+
+                        screenState=ScreenState.NORMAL;
+
+                        onRescale?.invoke(this);
+                    }
+
+                    stage.x=event.screenX-offsetX;
+                    stage.y=event.screenY-offsetY;
+                }
+                MouseEvent.MOUSE_PRESSED->{
+                    offsetX=event.sceneX;
+                    offsetY=event.sceneY;
+                }
+                MouseEvent.MOUSE_RELEASED->{
+                    adjustScreenState(event);
+                }
+            }
+        }
+
+
+        fun adjustScreenState(event: MouseEvent?)
+        {
+            if(desiredScreenState==screenState)
+                return;
+
+            if(event!=null)
+            {
+                previousOffsetX=event.sceneX;
+                previousOffsetY=event.sceneY;
+
+                previousWidth=stage.width;
+                previousHeight=stage.height;
+            }
+            else
+            {
+                previousOffsetX=stage.width*0.5;
+                previousOffsetY=20.0;
+            }
+
+            when(desiredScreenState)
+            {
+                ScreenState.FULL->{
+                    stage.x=Screen.getPrimary().visualBounds.minX;
+                    stage.y=Screen.getPrimary().visualBounds.minY;
+                    stage.width=Screen.getPrimary().visualBounds.minX+Screen.getPrimary().visualBounds.width;
+                    stage.height=Screen.getPrimary().visualBounds.minY+Screen.getPrimary().visualBounds.height;
+                }
+                ScreenState.LEFT->{
+                    stage.x=Screen.getPrimary().visualBounds.minX;
+                    stage.y=Screen.getPrimary().visualBounds.minY;
+                    stage.width=Screen.getPrimary().visualBounds.width*0.5;
+                    stage.height=Screen.getPrimary().visualBounds.height;
+                }
+                ScreenState.RIGHT->{
+                    stage.x=0.5*(Screen.getPrimary().visualBounds.maxX-Screen.getPrimary().visualBounds.minX);
+                    stage.y=Screen.getPrimary().visualBounds.minY;
+                    stage.width=Screen.getPrimary().visualBounds.width*0.5;
+                    stage.height=Screen.getPrimary().visualBounds.height;
+                }
+
+                ScreenState.NORMAL -> {
+                    stage.width=previousWidth;
+                    stage.height=previousHeight;
+                    stage.x=Screen.getPrimary().visualBounds.minX+Screen.getPrimary().visualBounds.width*0.5-stage.width*0.5;
+                    stage.y=Screen.getPrimary().visualBounds.minY+Screen.getPrimary().visualBounds.height*0.5-stage.height*0.5;
+
+                    screenState=ScreenState.NORMAL;
+                }
+            }
+
+            screenState=desiredScreenState;
+
+            onRescale?.invoke(this);
+        }
     }
 }
